f3bbfc8e10ed7af526657cb99b2ed5d7a4a5c213
Minh <tminh.us@gmail.com>
2024-08-29 20:41:46 -0700

    env setup

--- parameters/parameters.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass, field
-from typing import Optional, Callable
-
-@dataclass
-class RandomForestPipelineParameters:
-    rebalancer: Optional[Callable] = None
-    n_estimators: Optional[int] = None         # Number of trees in the forest
-    max_depth: Optional[int] = None            # Maximum depth of the trees
-    criterion: Optional[str] = None            # Function to measure the quality of a split (e.g., 'gini', 'entropy')
