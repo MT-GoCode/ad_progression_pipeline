10e934faf11b5393f2f834029a63622411144eac
Minh Trinh <tminh.us@gmail.com>
2024-09-06 08:10:27 -0700

    ref files

--- ad_progression_pipeline/components/models/tasks/rebalancing_random_forest.py
+++ ad_progression_pipeline/components/models/tasks/rebalancing_random_forest.py
@@ -12,45 +12,47 @@ from sklearn.preprocessing import StandardScaler
 from ad_progression_pipeline.utils.constants import RANDOM_SEED
 
 
-@task
-def train(df : pd.DataFrame) -> Pipeline:
+class RebalancingRandomForest:
 
-    log = get_run_logger()
+    @task
+    def train(self : "RebalancingRandomForest", df : pd.DataFrame) -> None:
 
-    x = df.drop(columns=["progression"])
-    y = df["progression"]
+        log = get_run_logger()
 
-    pipeline = Pipeline([
-        ("scaler", StandardScaler()),
-        ("smote", SMOTE(random_state=RANDOM_SEED)),
-        ("classifier", RandomForestClassifier(
-            random_state=RANDOM_SEED,
-            n_estimators=context.hyperparameters["n_estimators"],
-            max_depth=context.hyperparameters["max_depth"],
-            criterion=context.hyperparameters["criterion"],
-        )),
-    ])
+        x = df.drop(columns=["progression"])
+        y = df["progression"]
 
-    pipeline.fit(x, y)
+        self.pipeline = Pipeline([
+            ("scaler", StandardScaler()),
+            ("smote", SMOTE(random_state=RANDOM_SEED)),
+            ("classifier", RandomForestClassifier(
+                random_state=RANDOM_SEED,
+                n_estimators=context.hyperparameters["n_estimators"],
+                max_depth=context.hyperparameters["max_depth"],
+                criterion=context.hyperparameters["criterion"],
+            )),
+        ])
 
-    smote = pipeline.named_steps["smote"]
-    X_resampled, y_resampled = smote.fit_resample(x, y)
-    log.info(f"Original class distribution:{str(Counter(y))}")
-    log.info(f"Resampled class distribution: {str(Counter(y_resampled))}")
+        self.pipeline.fit(x, y)
 
-    return pipeline
+        smote = self.pipeline.named_steps["smote"]
+        X_resampled, y_resampled = smote.fit_resample(x, y)
+        log.info(f"Original class distribution:{str(Counter(y))}")
+        log.info(f"Resampled class distribution: {str(Counter(y_resampled))}")
 
+    @task
+    def infer_and_gather_metrics(self : "RebalancingRandomForest", df : pd.DataFrame) -> tuple[Any, dict]:
+        x = df.drop(columns=["progression"])
+        y = df["progression"]
 
-@task
-def infer_and_gather_metrics(pipeline : Pipeline, df : pd.DataFrame) -> tuple[Any, dict]:
-    x = df.drop(columns=["progression"])
-    y = df["progression"]
+        y_pred = self.pipeline.predict(x)
 
-    y_pred = pipeline.predict(x)
+        return y_pred, {
+            "accuracy" : accuracy_score(y, y_pred),
+            "precision" : precision_score(y, y_pred, average="binary"),
+            "recall" : recall_score(y, y_pred, average="binary"),
+            "f1" : f1_score(y, y_pred, average="binary"),
+        }
 
-    return y_pred, {
-        "accuracy" : accuracy_score(y, y_pred),
-        "precision" : precision_score(y, y_pred, average="binary"),
-        "recall" : recall_score(y, y_pred, average="binary"),
-        "f1" : f1_score(y, y_pred, average="binary"),
-    }
+    # @task
+    # def serialize(self, output_dir) -> Pipeline:
